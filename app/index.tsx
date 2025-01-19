571515c8eb2e103fa2cbe01ce7818a927ffe2414
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-19 20:04:55 +0100

    Update inddex.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,38 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
 import { useRouter } from "expo-router";
+import { Colors } from "@/constants/Colors";
+import Animated from "react-native-reanimated";
 
 const Page = () => {
   const router = useRouter();
 
   return (
     <View style={styles.container}>
-      <Text>Welcome Page</Text>
-      <TouchableOpacity onPress={() => router.replace("/(tabs)")}>
-        <Text>Go to Home Screen</Text>
-      </TouchableOpacity>
+      <ImageBackground
+        source={require("@/assets/images/getting-started.jpg")}
+        style={{ flex: 1 }}
+        resizeMode="cover"
+      >
+        <View style={styles.wrapper}>
+          <Animated.Text style={styles.title}>Stay updated</Animated.Text>
+          <Animated.Text style={styles.description}>
+            Get breaking news and personalized updates directly to your feed.{" "}
+          </Animated.Text>
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={() => router.replace("/(tabs)")}
+          >
+            <Animated.Text style={styles.btnText}>Get started</Animated.Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
@@ -22,5 +44,41 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  wrapper: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingBottom: 50,
+    paddingHorizontal: 30,
+    gap: 10,
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  title: {
+    color: Colors.white,
+    fontSize: 24,
+    fontWeight: "600",
+    letterSpacing: 1,
+    lineHeight: 36,
+    textAlign: "center",
+  },
+  description: {
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: "500",
+    lineHeight: 22,
+    letterSpacing: 1.2,
+    textAlign: "center",
+  },
+  btn: {
+    backgroundColor: Colors.tint,
+    paddingVertical: 15,
+    marginVertical: 20,
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  btnText: {
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: "700",
   },
 });
